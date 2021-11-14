0db2fbd258292475c1e8611c31aa5f2f8050d4cd
Xurxo <xfresco@adeqsystems.es>
2021-11-14 21:57:40 +0100

    F# solution

---
+++ soluciones/xurxof/miniAoC/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+open reto1
+
+let email_adresses = [
+    "luis@hackeros.com";
+    "pepe@outlook.com";
+    "xurxo@hackeros.com";
+    "elena@pradorramisquedo.com";
+    "patricia@hackeros.com";
+    "salome@hackeros.com";
+    "andres@dprimo.com"
+]
+
+
+[<EntryPoint>]
+let main argv =
+    let result = repeatedDomain  email_adresses
+    
+    printfn "%s" result
+     
+    0 // return an integer exit code
