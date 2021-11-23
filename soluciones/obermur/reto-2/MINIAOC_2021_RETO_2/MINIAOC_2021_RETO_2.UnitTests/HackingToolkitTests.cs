1d2da545a9392e88873fc01f3064bf1645cff7c3
Oliver Bermudez Muruzabal <obm@Olivers-MacBook-Pro.local>
2021-11-24 00:59:36 +0100

    Reto 2 ready

---
+++ soluciones/obermur/reto-2/MINIAOC_2021_RETO_2/MINIAOC_2021_RETO_2.UnitTests/HackingToolkitTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace MINIAOC_2021_RETO_2.UnitTests;
+
+public class HackingToolkitTests{
+    [TestCase("ab", 2 * 1)]
+    [TestCase("abc", 3 * 2 * 1)]
+    [TestCase("abcd", 4 * 3 * 2 * 1)]
+    [TestCase("abcde", 5 * 4 * 3 * 2 * 1)]
+    public void GenerateTrollPassword_SpecificSample_ReturnsDictionaryWithSpecificNumberOfItems(string sample,int length){
+        //Assert
+        HackingToolkit sut = new();
+        //Act
+        string[] dictionary = sut.GenerateTrollPassword(sample);
+        //Assert
+        Assert.IsNotNull(dictionary);
+        Assert.AreEqual(length, dictionary.Length);
+    }
+    [TestCase("ab", "ab")]
+    [TestCase("ab", "ba")]
+    [TestCase("abc", "abc")]
+    [TestCase("abc", "acb")]
+    [TestCase("abc", "bac")]
+    [TestCase("abc", "bca")]
+    [TestCase("abc", "cab")]
+    [TestCase("abc", "cba")]
+    public void GenerateTrollPassword_SpecificSample_ReturnsDictionaryThatContainsSpecificItem(string sample,string item){
+        //Assert
+        HackingToolkit sut = new();
+        //Act
+        string[] dictionary = sut.GenerateTrollPassword(sample);
+        //Assert
+        Assert.IsNotNull(dictionary);
+        Assert.Contains(item, dictionary);
+    }
+}
---
+++ soluciones/obermur/reto-2/MINIAOC_2021_RETO_2/MINIAOC_2021_RETO_2/HackingToolkit.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace MINIAOC_2021_RETO_2{
+    public class HackingToolkit{
+        public string[] GenerateTrollPassword(string sample){
+            List<int[]> references = Generate(sample.Length);
+            return GetDictionary(sample, references);
+        }
+        private string[] GetDictionary(string sample, List<int[]> references){
+            List<string> dictionary = new();
+            char[] items = sample.ToCharArray();
+            foreach(int[] reference in references){
+                string item = Generate(items, reference);
+                dictionary.Add(item);
+            }
+            return dictionary.ToArray();
+        }
+        private static string Generate(char[] items, int[] reference){
+            StringBuilder builder = new();
+            foreach(int index in reference){
+                builder.Append(items[index]);
+            }
+            return builder.ToString();
+        }
+        private List<int[]> Generate(int length){
+            List<int> items = new();
+            for(int i = 0; i < length; i++){
+                items.Add(i);
+            }
+            return Generate(Array.Empty<int>(), items.ToArray());
+        }
+        private List<int[]> Generate(int[] selection, int[] remains) {
+            if (remains.Length > 1){
+                List<int[]> solution = new();
+                for (int i = 0; i < remains.Length; i++){
+                    List<int> newSelection = new(selection);
+                    newSelection.Add(remains[i]);
+                    List<int> newRemains = new(remains);
+                    newRemains.RemoveAt(i);
+                    List<int[]> list = Generate(newSelection.ToArray(), newRemains.ToArray());
+                    solution.AddRange(list);
+                }
+                return solution;
+            }else{
+                List<int> newSelection = new(selection);
+                newSelection.Add(remains[0]);
+                List<int[]> result = new();
+                result.Add(newSelection.ToArray());
+                return result;
+            }
+        }
+    }
+}
+
