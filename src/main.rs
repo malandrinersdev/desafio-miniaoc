5898bc3364ffcb02b2f121fa256eb6279b6c6187
Álex Hermida <alexhermida@gmail.com>
2021-11-23 23:18:29 +0100

    Test passing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use counter::Counter;
 
-pub fn find_smart_hackers(email_list: &[String]) -> [String] {
-    return ["l"]
+pub fn find_smart_hackers<'a>(email_list: &'a[&'a str]) -> Vec<&str> {
+
+    let mut domain_list = Vec::new();
+    for x in email_list {
+        let domain_index = x.find("@").unwrap();
+        let slice = &x[domain_index..];
+        domain_list.push(slice);
+    }
+    let most_common = domain_list.iter().collect::<Counter<_>>().most_common_ordered();
+    let domain_spam = most_common[0].0;
+    let filtered_list: Vec<&str> = email_list.iter().filter(|&x| x.contains(domain_spam)).cloned().collect();
+
+    filtered_list
 }
 
 
@@ -17,17 +29,27 @@ mod tests {
             "coco@malandriners.dev",
             "hello@somoshackersastutos.com",
             "ambrosio@outlook.com",
-            "ciao@somoshackersastutos.com"
+            "ciao@somoshackersastutos.com",
         ];
         let result_list = [
             "hola@somoshackersastutos.com",
             "hello@somoshackersastutos.com",
-            "ciao@somoshackersastutos.com"
+            "ciao@somoshackersastutos.com",
         ];
         assert_eq!(find_smart_hackers(&input_emails), result_list);
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    let input_emails = [
+        "hola@somoshackersastutos.com",
+        "ambrosio@outlook.com",
+        "coco@malandriners.dev",
+        "hello@somoshackersastutos.com",
+        "ambrosio@outlook.com",
+        "ciao@somoshackersastutos.com",
+    ];
+
+    let hackers_emails = find_smart_hackers(&input_emails);
+    println!("{:?}", hackers_emails);
 }
