3a9412ccf1e3cb3b77becf357325c1b077bc5cb4
Xurxo <xfresco@adeqsystems.es>
2021-11-23 23:08:29 +0100

    Reto 2: F# solution

---
+++ soluciones/xurxof/Reto2/GenerateTrollPassword.fs
@@ -0,0 +1,26 @@
+﻿module GenerateTrollPassword
+
+
+let rec moveCharToStart (input:string) (i:int) = 
+    let startChar = input.[i..i]
+    let reminder = input.[0 .. i-1] + input.[i+1 .. input.Length]
+    match reminder.Length with 
+        | 1 | 0 -> [startChar+reminder]
+        | _ -> 
+            let mixedReminder = mix reminder
+            let result = 
+                mixedReminder 
+                |> Seq.map (fun u-> startChar + u)
+                |> Seq.toList
+            result
+
+and mix input = 
+    let result = [0 .. -1 + Seq.length input] 
+                |> Seq.map (fun i -> moveCharToStart input i )
+                |> Seq.fold (fun a b -> a @ b) []
+    result
+
+let generateTrollPassword input = 
+    match String.length input with
+    | 1 ->[ input ]
+    | _ -> mix input 
---
+++ soluciones/xurxof/Reto2/testing.fs
@@ -0,0 +1,32 @@
+﻿module testing
+
+open Xunit
+open GenerateTrollPassword
+
+[<Fact>]
+let generateTrollPassword_OneElement_ReturnsElement () =
+    Assert.True(generateTrollPassword "a" = [ "a" ])
+
+[<Fact>]
+let mix_ThreeElement_ReturnsElement () =
+    let result = mix "abc" 
+    let expect = ["abc";"acb";"bac";"bca";"cab";"cba"]
+    Assert.True ((expect = result))
+
+[<Fact>]
+let mix_TwoElement_ReturnsElement () =
+    let result = mix "ab" 
+    let expect = ["ab"; "ba"]
+    Assert.True ((expect = result))
+
+[<Fact>]
+let mix_OneElement_ReturnsElement () =
+    let result = mix "a" 
+    let expect = ["a"]
+    Assert.True ((expect = result))
+
+[<Fact>]
+let mix_EmptyElement_ReturnsElement () =
+    let result = mix "" 
+    let expect = []
+    Assert.True ((expect = result))
