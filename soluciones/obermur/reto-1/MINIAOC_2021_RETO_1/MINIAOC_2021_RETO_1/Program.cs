a24b397419fe698a8a9ff265b3154ada28a3fe71
Oliver Bermudez Muruzabal <obm@Olivers-MacBook-Pro.local>
2021-11-23 01:17:45 +0100

    Reto 1 ready

---
+++ soluciones/obermur/reto-1/MINIAOC_2021_RETO_1/MINIAOC_2021_RETO_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq;
+using MINIAOC_2021_RETO_1;
+
+Console.WriteLine("Hello, World!");
+
+string[] input = new[] {
+    "hola@somoshackersastutos.com",
+    "ambrosio@outlook.com",
+    "coco@malandriners.dev",
+    "hello@somoshackersastutos.com",
+    "ambrosio@outlook.com",
+    "ciao@somoshackersastutos.com"
+};
+
+HackerDetector hackerDetector = new();
+string[] hackers = hackerDetector.FindSmartHackers(input);
+
+string output = $"['{String.Join("','", hackers)}']";
+Console.WriteLine(output);
