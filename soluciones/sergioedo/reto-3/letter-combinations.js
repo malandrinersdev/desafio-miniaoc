9822fb8b2b6daf962ce9654a72a2aabc823e1504
sergioedo <sergio.edo@gmail.com>
2021-11-29 21:13:42 +0100

    3.1 tests passed

--- soluciones/sergioedo/reto-3/letter-combinations.js
+++ soluciones/sergioedo/reto-3/letter-combinations.js
@@ -1,4 +1,33 @@
+const digits2Chars = {
+    0: " ",
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "x", "y", "z"],
+}
+
 const letterCombinations = (digits) => {
+    console.log(digits)
+    digitsArray = digits.toString().split("")
+    if (digitsArray.length === 1) {
+        return digits2Chars[digitsArray[0]]
+    } else {
+        const firstDigit = digitsArray[0]
+        const subDigits = digitsArray.slice(1).join("")
+        const firstDigitChars = digits2Chars[firstDigit]
+        const subCombinations = letterCombinations(subDigits)
+        return firstDigitChars
+            .map((char) => {
+                return subCombinations.map((subCombination) => {
+                    return char + subCombination
+                })
+            })
+            .flat()
+    }
 }
 
 module.exports = letterCombinations
