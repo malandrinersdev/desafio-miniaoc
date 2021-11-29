4c8ef15771f0e5d74bc985b12cecad30966bf3f6
Emilio Diez <ediezf@minsait.com>
2021-11-29 21:14:43 +0100

    Reto 3: Nokia 6310 - 2ª Parte

--- soluciones/ejdiezfraile/reto-3/reto-3.js
+++ soluciones/ejdiezfraile/reto-3/reto-3.js
@@ -1,5 +1,5 @@
 
-const letters = {
+const digitMap = {
     '2': ['a', 'b', 'c'],
     '3': ['d', 'e', 'f'],
     '4': ['g', 'h', 'i'],
@@ -7,7 +7,8 @@ const letters = {
     '6': ['m', 'n', 'o'],
     '7': ['p', 'q', 'r', 's'],
     '8': ['t', 'u', 'v'],
-    '9': ['w', 'x', 'y', 'z']
+    '9': ['w', 'x', 'y', 'z'],
+    '0': [' ']
 }
 
 function letterCombinations(digits) {
@@ -17,13 +18,13 @@ function letterCombinations(digits) {
     // Zero or One char length (base case)
     if (digits.length <= 1) {
         // Empty array if undefined
-        return (letters[digits.charAt(0)] || [])
+        return (digitMap[digits.charAt(0)] || [])
     }
 
     // f(n) = f(char(0)) + f(char(1..n)) 
     const outputFirst = letterCombinations(digits.charAt(0))
     const outputOthers = letterCombinations(digits.substring(1))
-    
+
     // Combine both outputs (first + others until the end)
     const combinedOutput = []
     outputFirst.forEach(itemFromFirst => {
@@ -34,4 +35,16 @@ function letterCombinations(digits) {
     return combinedOutput
 }
 
+function keysPressed(sentence) {
+    const letters = sentence.split('')
+
+    // Iterate over digitMap keys to find every letter
+    return letters.flatMap(letter =>
+        Object.keys(digitMap)
+            .filter(digit =>
+                digitMap[digit]
+                    .find(char => char === letter)))
+}
+
 exports.letterCombinations = letterCombinations;
+exports.keysPressed = keysPressed;
--- soluciones/ejdiezfraile/reto-3/test/reto-3.test.js
+++ soluciones/ejdiezfraile/reto-3/test/reto-3.test.js
@@ -13,13 +13,23 @@ test('digit 6 with result m, n, o', () => {
     expect(reto.letterCombinations(6)).toStrictEqual(['m', 'n', 'o']);
 });
 
-test('digit 23 with result in 9 combinations', () => {
+test('digits 23 with result in 9 combinations', () => {
     expect(reto.letterCombinations(23)).toStrictEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
 });
 
-test('digit 234 with result in 27 combinations', () => {
+test('digits 234 with result in 27 combinations', () => {
     expect(reto.letterCombinations(234)).toStrictEqual([
         'adg', 'adh', 'adi', 'aeg', 'aeh', 'aei', 'afg', 'afh', 'afi', 
         'bdg', 'bdh', 'bdi', 'beg', 'beh', 'bei', 'bfg', 'bfh', 'bfi', 
         'cdg', 'cdh', 'cdi', 'ceg', 'ceh', 'cei', 'cfg', 'cfh', 'cfi']);
+});
+
+
+test('sentence "m "  result in 6, 0', () => {
+    expect(reto.keysPressed('m ')).toStrictEqual(['6', '0']);
+});
+
+test('sentence "malo olma loma mola" result in reto solution', () => {
+    expect(reto.keysPressed('malo olma loma mola')).toStrictEqual(
+        ['6', '2', '5', '6', '0', '6', '5', '6', '2', '0', '5', '6', '6', '2', '0', '6', '6', '5', '2']);
 });
