1905e596f021b30a67a03f9f0efc9c320dc9f008
Emilio Diez <ediezf@minsait.com>
2021-11-29 18:01:30 +0100

    Reto 3: Nokia 6310 - 1ª Parte

--- soluciones/ejdiezfraile/reto-3/reto-3.js
+++ soluciones/ejdiezfraile/reto-3/reto-3.js
@@ -1,8 +1,37 @@
-function letterCombinations(digits) {
-    digits = digits || ''
-    const output = ['']
 
-    return output
+const letters = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+}
+
+function letterCombinations(digits) {
+    // Convert to string
+    digits = '' + (digits || '')
+
+    // Zero or One char length (base case)
+    if (digits.length <= 1) {
+        // Empty array if undefined
+        return (letters[digits.charAt(0)] || [])
+    }
+
+    // f(n) = f(char(0)) + f(char(1..n)) 
+    const outputFirst = letterCombinations(digits.charAt(0))
+    const outputOthers = letterCombinations(digits.substring(1))
+    
+    // Combine both outputs (first + others until the end)
+    const combinedOutput = []
+    outputFirst.forEach(itemFromFirst => {
+        outputOthers.forEach(itemFromOthers => {
+            combinedOutput.push(itemFromFirst + itemFromOthers)
+        })
+    });
+    return combinedOutput
 }
 
 exports.letterCombinations = letterCombinations;
--- soluciones/ejdiezfraile/reto-3/test/reto-3.test.js
+++ soluciones/ejdiezfraile/reto-3/test/reto-3.test.js
@@ -1,5 +1,25 @@
 const reto = require('../reto-3')
 
-test('null digit with result in empty string array output', () => {
-    expect(reto.letterCombinations(null)).toStrictEqual(['']);
+test('null digit with result in an empty array output', () => {
+    expect(reto.letterCombinations(null)).toStrictEqual([]);
+});
+
+test('no valid digit with result in an empty array output', () => {
+    expect(reto.letterCombinations(1)).toStrictEqual([]);
+});
+
+
+test('digit 6 with result m, n, o', () => {
+    expect(reto.letterCombinations(6)).toStrictEqual(['m', 'n', 'o']);
+});
+
+test('digit 23 with result in 9 combinations', () => {
+    expect(reto.letterCombinations(23)).toStrictEqual(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf']);
+});
+
+test('digit 234 with result in 27 combinations', () => {
+    expect(reto.letterCombinations(234)).toStrictEqual([
+        'adg', 'adh', 'adi', 'aeg', 'aeh', 'aei', 'afg', 'afh', 'afi', 
+        'bdg', 'bdh', 'bdi', 'beg', 'beh', 'bei', 'bfg', 'bfh', 'bfi', 
+        'cdg', 'cdh', 'cdi', 'ceg', 'ceh', 'cei', 'cfg', 'cfh', 'cfi']);
 });
