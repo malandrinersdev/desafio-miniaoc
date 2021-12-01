faf375e5ea55c9f2d8cf8495649b9207b5e2dde1
Álex Hermida <alexhermida@gmail.com>
2021-12-02 00:09:56 +0100

    Add very basic terminal commands for testing and final styles fixes

--- soluciones/alexhermida/reto-3-python/README.md
+++ soluciones/alexhermida/reto-3-python/README.md
@@ -13,8 +13,25 @@ $python -m venv .venv && .venv/bin/activate && pip install -r requirements.txt
 
 Ejecutar código:
 
+**No se validan posibles entradas de datos en el input de línea de comandos.**
+
+
+letter_combinations:
+
 ```bash
-$python main.py
+$python main.py --letter_combinations
+```
+
+find_string_keypress:
+
+```bash
+$python main.py --find_string_keypress
+```
+
+Obtener ayuda:
+
+```bash
+$python main.py -h
 ```
 
 Ejecutar tests:
--- soluciones/alexhermida/reto-3-python/main.py
+++ soluciones/alexhermida/reto-3-python/main.py
@@ -14,7 +14,13 @@ mobile_digits = {
 }
 
 
-def find_combinations(mobile_digits: Dict, input_digits: List[int], combinations: List, position: int, result: str =''):
+def find_combinations(
+    mobile_digits: Dict,
+    input_digits: List[int],
+    combinations: List,
+    position: int,
+    result: str = "",
+):
     ###
     # Recursive function to iterate over the input digits with the mobile digits starting from the last one.
     ##
@@ -22,33 +28,44 @@ def find_combinations(mobile_digits: Dict, input_digits: List[int], combinations
         combinations.append(result)
         return
 
-    digit = input_digits[position-1]
+    digit = input_digits[position - 1]
     digit_options = mobile_digits[digit]
     options_length = len(digit_options)
 
     for i in range(options_length):
-        find_combinations(mobile_digits, input_digits, combinations, position-1, digit_options[i] + result)
+        find_combinations(
+            mobile_digits,
+            input_digits,
+            combinations,
+            position - 1,
+            digit_options[i] + result,
+        )
 
 
 def letter_combinations(digits: int) -> List[str]:
     digits_list = [int(d) for d in str(digits)]
     combinations: List[str] = []
 
-    find_combinations(mobile_digits=mobile_digits, input_digits=digits_list, combinations=combinations, position=len(digits_list))
+    find_combinations(
+        mobile_digits=mobile_digits,
+        input_digits=digits_list,
+        combinations=combinations,
+        position=len(digits_list),
+    )
 
     return combinations
 
 
 def helper_char_to_keypress(mobile_digits: Dict):
-    keypress_dict: Dict[str,str] = {}
+    keypress_dict: Dict[str, str] = {}
     for key, v in mobile_digits.items():
         for i in range(len(v)):
-            n_keypress = "".join([str(key) for _ in range(i+1)])
+            n_keypress = "".join([str(key) for _ in range(i + 1)])
             keypress_dict.setdefault(v[i], n_keypress)
     return keypress_dict
 
 
-def find_string_keypress(input_text:str) -> str:
+def find_string_keypress(input_text: str) -> str:
     keypress_str = ""
 
     for char in input_text:
@@ -56,3 +73,35 @@ def find_string_keypress(input_text:str) -> str:
         keypress_str += keypress_dict[char]
 
     return keypress_str
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--letter_combinations",
+        action="store_true",
+        help="Dada una serie de dígitos, mostrar todas \
+                        las combinaciones posibles de letras que se pueden conseguir.",
+    )
+    parser.add_argument(
+        "--find_string_keypress",
+        action="store_true",
+        help="Averiguar la combinación de teclas posible data una frase",
+    )
+    args = parser.parse_args()
+
+    if args.letter_combinations:
+        digits = int(
+            input(
+                "Introduce los dígitos, del 2 al 9, para obtener las combinaciones posibles:"
+            )
+        )
+        print(letter_combinations(digits))
+
+    elif args.find_string_keypress:
+        keypress = input(
+            "Introduce una frase y obten las pulsaciones de teclas requeridas en un móvil malandriner:"
+        )
+        print(find_string_keypress(keypress))
